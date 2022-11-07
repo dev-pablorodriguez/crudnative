1fa4890275266ec9eed7884107d62fe39527be76
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-07 01:28:38 -0300

    Instalación React Native Paper y dependencias

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
 
 //React Navigation
 import { NavigationContainer } from '@react-navigation/native'
@@ -11,11 +12,26 @@ import Inicio from './views/Inicio'
 import NuevoCliente from './views/NuevoCliente'
 import DetalleCliente from './views/DetalleCliente'
 
+//Definir el tema
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF'
+  }
+}
+
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName='Inicio'
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.colors.primary },
+          headerTintColor: theme.colors.surface,
+          headerTitleStyle: { fontWeight: 'bold' }
+      }}
       >
         <Stack.Screen
           name='Inicio'
