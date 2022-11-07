6618b4f011a18ea7ead7ae7c9a42571d77f6571c
Pablo Rodríguez <pablo.rodriguez.musre@gmail.com>
2022-11-07 01:06:49 -0300

    Creación y configuración vistas

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
+//React Navigation
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+import Inicio from './views/Inicio'
+import NuevoCliente from './views/NuevoCliente'
+import DetalleCliente from './views/DetalleCliente'
+
 const App = () => {
   return (
-    <>
-
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Inicio'
+      >
+        <Stack.Screen
+          name='Inicio'
+          component={ Inicio }
+        />
+        <Stack.Screen
+          name='NuevoCliente'
+          component={ NuevoCliente }
+          options={{
+            title: 'Nuevo Cliente'
+          }}
+        />
+        <Stack.Screen
+          name='DetalleCliente'
+          component={ DetalleCliente }
+          options={{
+            title: 'Detalle Cliente'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
